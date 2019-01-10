c88e9f863c9980e3fe7442829eb310c7593aeb0b
Florian Keller <github@floriankeller.de>
2019-01-10 18:13:25 +0100

    Updates

---
+++ precache-manifest.a1c465c42425d684a4cbffc27def7cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fcbdaa95a856936643af",
+    "url": "/static/js/main.fcbdaa95.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "fcbdaa95a856936643af",
+    "url": "/static/css/main.63285eb2.chunk.css"
+  },
+  {
+    "revision": "143372c3fd1ea51765f6862f74aece07",
+    "url": "/index.html"
+  }
+];
