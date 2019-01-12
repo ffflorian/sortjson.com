d0bd6570b1e1cd567c452de557c4e7e736286423
Florian Keller <github@floriankeller.de>
2019-01-12 10:47:48 +0100

    chore: Copy CNAME file when building

--- bin/static-files.js
+++ bin/static-files.js
@@ -5,11 +5,11 @@ const path = require('path');
 
 const BUILD_DIR = path.resolve(__dirname, '..', 'build');
 
-const CNAME_FILE = path.join(BUILD_DIR, 'CNAME');
+const CNAME_ORIGINAL_FILE = path.join(__dirname, '..', 'CNAME');
+const CNAME_DEST_FILE = path.join(BUILD_DIR, 'CNAME');
 const ROBOTS_FILE = path.join(BUILD_DIR, 'robots.txt');
 
-const CNAME_CONTENT = 'www.sortjson.com';
 const ROBOTS_CONTENT = 'User-agent: *\nAllow: /';
 
-fs.writeFileSync(CNAME_FILE, CNAME_CONTENT, 'utf8');
 fs.writeFileSync(ROBOTS_FILE, ROBOTS_CONTENT, 'utf8');
+fs.copyFileSync(CNAME_ORIGINAL_FILE, CNAME_DEST_FILE);
