6025c74d3045d67cd519727af585e43d2c580720
Florian Keller <github@floriankeller.de>
2019-01-12 10:36:42 +0100

    feat: Show version from package.json

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,13 @@
 import {Typography} from '@material-ui/core';
 import * as React from 'react';
 
+const version = process.env.REACT_APP_VERSION || '';
+
 class Footer extends React.Component {
   render() {
     return (
       <Typography style={{margin: 10}} gutterBottom={true} align="left">
-        {`Version 1.0`}
+        {`Version ${version}`}
       </Typography>
     );
   }
