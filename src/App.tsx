32083b88cb2d444e5df8b78b632f571db38a2458
AndyLnd <andy@wire.com>
2019-02-06 14:36:33 +0100

    sfc app

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import {AppProvider} from './AppProvider';
 import {AppTheme} from './AppTheme';
 import {Content, Footer, Header} from './components/layout/';
 
-class App extends React.Component {
-  render() {
-    return (
-      <AppProvider>
-        <AppTheme>
-          <Header />
-          <Content />
-          <Footer />
-        </AppTheme>
-      </AppProvider>
-    );
-  }
-}
+const App = () => (
+  <AppProvider>
+    <AppTheme>
+      <Header />
+      <Content />
+      <Footer />
+    </AppTheme>
+  </AppProvider>
+);
 
 export default App;
