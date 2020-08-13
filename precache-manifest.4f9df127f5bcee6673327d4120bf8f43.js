18bc2b9b127523b032c571d09be10c48a3f6eaf6
ffflobot <ffflobot@users.noreply.github.com>
2020-08-13 16:00:08 +0000

    Publish v1.5.6

--- precache-manifest.4f9df127f5bcee6673327d4120bf8f43.js
+++ precache-manifest.4f9df127f5bcee6673327d4120bf8f43.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3516afc3e7ba1d12684330821ef3c396",
+    "revision": "d938de708e02d08bb6ba95e231ccdbae",
     "url": "https://sortjson.com/index.html"
   },
   {
-    "revision": "b340ac9149afe8b94a3d",
+    "revision": "d749f470c8c178fdc8f5",
     "url": "https://sortjson.com/static/css/main.6acd7ade.chunk.css"
   },
   {
-    "revision": "5ee5b614564b7e720dbc",
-    "url": "https://sortjson.com/static/js/2.e3ade1a3.chunk.js"
+    "revision": "9c27321651ffa997961a",
+    "url": "https://sortjson.com/static/js/2.b83b5a7b.chunk.js"
   },
   {
     "revision": "830a48a70ae712b864346517f50e1297",
-    "url": "https://sortjson.com/static/js/2.e3ade1a3.chunk.js.LICENSE.txt"
+    "url": "https://sortjson.com/static/js/2.b83b5a7b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b340ac9149afe8b94a3d",
-    "url": "https://sortjson.com/static/js/main.954dc563.chunk.js"
+    "revision": "d749f470c8c178fdc8f5",
+    "url": "https://sortjson.com/static/js/main.32327eb1.chunk.js"
   },
   {
     "revision": "3f7b293aeb5128fb3263",
